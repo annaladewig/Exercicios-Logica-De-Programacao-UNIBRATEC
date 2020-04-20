8d9b4a82c9ea979a21513097dafcb9995a34fb5e
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-04-20 06:27:27 -0300

    Adicionei o Exercício 04

---
+++ 1º Unidade/Exercício04-MultaDiaria/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício04_MultaDiaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dias;
+            const double multa = 1.5;
+            double total;
+
+            Console.Write("Quantos foram os dias de atraso? ");
+            dias = Convert.ToInt32(Console.ReadLine());
+            total = dias * multa;
+            Console.Write("A multa será de R$ " + total);
+            Console.ReadKey();
+        }
+    }
+}
