626f67f4d80ad1c9adc57698ecb83193ac484b20
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-04-20 06:20:12 -0300

    Adicionei o Exercício 03

---
+++ 1º Unidade/Exercício03-TotaisDeNumeros/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício03_TotaisDeNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, somaInt;
+            double num3, num4, somaReais, somaTotal;
+
+            Console.Write("Digite um número inteiro: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite um segundo número inteiro: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite um número real: ");
+            num3 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite um segundo número real: ");
+            num4 = Convert.ToDouble(Console.ReadLine());
+            somaInt = num1 + num2;
+            somaReais = num3 + num4;
+            somaTotal = somaInt + somaReais;
+            Console.WriteLine("A soma dos números inteiros é " + somaInt);
+            Console.WriteLine("A soma dos números reais é " + somaReais);
+            Console.WriteLine("A soma de todos os números é " + somaTotal);
+            Console.ReadKey();
+        }
+    }
+}
