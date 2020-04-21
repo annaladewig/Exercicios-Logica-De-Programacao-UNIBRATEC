302280261bb9144dcb70f90594e27bc3df56a9ad
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-04-21 12:01:17 -0300

    Adicionei o Exercício 06

---
+++ 1º Unidade/Exercício06-CalculoDaAreaDoCirculo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício06_CalculoDaAreaDoCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raioCirculo, areaCirculo;
+            const double pi = 3.14;
+            Console.Write("Qual o tamanho do raio do círculo em centímetros? ");
+            raioCirculo = Convert.ToDouble(Console.ReadLine());
+            areaCirculo = pi * (raioCirculo * raioCirculo);
+            Console.WriteLine("A área do cículo é de {0} cm.", areaCirculo);
+            Console.ReadKey();
+        }
+    }
+}
