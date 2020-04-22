4dda8d2a72377f25abc5ffa7041355835c3f12fa
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-04-22 14:41:26 -0300

    Adicionei o Exercício 07

---
+++ 1º Unidade/Exercício07-ConversaoDeGraus/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício07_ConversaoDeGraus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tempFahrenheit, tempCelsius;
+
+            Console.Write("Qual a temperatura em graus Fahrenheit? ");
+            tempFahrenheit = Convert.ToDouble(Console.ReadLine());
+            tempCelsius = (tempFahrenheit - 32) * 5 / 9;
+            Console.WriteLine("A temperatura de {0}°F equivale a {1}°C.", tempFahrenheit, tempCelsius);
+            Console.ReadKey();
+
+          
+        }
+    }
+}
