22ccb9db73846216d58a4e56432c9ba38caa102b
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-04-24 15:22:32 -0300

    Adicionei o Exercício 10

---
+++ 1º Unidade/Exercício10-OperadorTexto/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício10_OperadorTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string A, B, C, S, D, E, F, G;
+
+            A = "I";
+            B = "BRA";
+            C = "TEC";
+            S = "X";
+
+            D = A + B + C;
+            Console.WriteLine(D);
+
+            E = B + "SIL";
+            Console.WriteLine(E);
+
+            F = C + "NO" + B + "S";
+            Console.WriteLine(F);
+
+            G = C + "NO" + B + S;
+            Console.WriteLine(G);
+
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
