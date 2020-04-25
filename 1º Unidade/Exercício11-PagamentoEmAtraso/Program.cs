27ec1832d2a9181da9d49b6bfbccc14353325638
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-04-25 10:23:46 -0300

    Adicionei o Exercício 11

---
+++ 1º Unidade/Exercício11-PagamentoEmAtraso/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício11_PagamentoEmAtraso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entrada
+
+            double valorParcela, valorJurosDia, valorMulta, valorTotalJuros, valorEncargo, valorPagamento;
+            int diasAtraso;
+
+            Console.Write("Qual o valor da parcela? R$ ");
+            valorParcela = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Quantos dias de atraso? ");
+            diasAtraso = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Qual valor dos juros por dia de atraso? R$ ");
+            valorJurosDia = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Qual o valor da multa? R$ ");
+            valorMulta = Convert.ToDouble(Console.ReadLine());
+
+            // Processamento
+
+            valorTotalJuros = diasAtraso * valorJurosDia;
+            valorEncargo = valorTotalJuros + valorMulta;
+            valorPagamento = valorParcela + valorEncargo;
+
+            //Saída
+
+            Console.WriteLine("O valor total dos juros é de R$ {0:N}", valorTotalJuros);
+            Console.WriteLine("O valor total do encargo é de R$ {0:N}", valorEncargo);
+            Console.WriteLine("O valor total do pagamento é de R$ {0:N}", valorPagamento);
+            Console.ReadKey();      
+        }
+
+
+    }
+}
