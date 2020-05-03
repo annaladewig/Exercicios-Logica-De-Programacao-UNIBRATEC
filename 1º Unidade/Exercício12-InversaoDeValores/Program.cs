da88fc93f700540f694927c2eb8166080a367469
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-05-03 07:37:37 -0300

    Adicionei o Exercício 12

---
+++ 1º Unidade/Exercício12-InversaoDeValores/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício12_InversaoDeValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entrada
+
+            int primeiro, segundo, temp;
+
+            try
+            {
+                Console.Write("Digite o primeiro número: ");
+                primeiro = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Digite o segundo número: ");
+                segundo = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            catch
+            {
+                Console.Write("Por favor, digite apenas NÚMEROS.");
+                Console.ReadKey();
+                return;
+
+            }
+
+            // Processamento
+
+            temp = primeiro;
+            primeiro = segundo;
+            segundo = temp;
+
+
+            // Saída
+
+            Console.WriteLine("O valor do primeiro número é: " + primeiro);
+            Console.WriteLine("O valor do segundo número é: " + segundo);
+            Console.ReadKey();
+        }
+    }
+}
