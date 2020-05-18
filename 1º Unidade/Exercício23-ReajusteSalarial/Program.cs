ff10b2a218256a88d1835b4f6fb765cb64bdd305
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-05-18 20:26:04 -0300

    Adicionei o Exercício 23

---
+++ 1º Unidade/Exercício23-ReajusteSalarial/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício23_ReajusteSalarial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entradas
+            double salarioMensal, valorReajuste, novoSalario;
+
+            try
+            {
+                Console.Write("Qual o valor mensal do salário? R$ ");
+                salarioMensal = Convert.ToDouble(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Por favor digite apenas números.");
+                Console.ReadKey();
+                return;
+            }
+            try
+
+            {
+                Console.Write("Qual o percentual de reajuste? ");
+                valorReajuste = Convert.ToDouble(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Por favor, digite apenas números e sem o símbolo de porcentagem(%).");
+                Console.ReadKey();
+                return;
+            }
+
+            //Processamento
+            novoSalario = salarioMensal + salarioMensal * valorReajuste / 100;
+
+            //Saídas
+            Console.WriteLine("O valor do nova salário é de R$ {0:N}.", novoSalario);
+            Console.ReadKey();
+
+            }
+    }
+}
