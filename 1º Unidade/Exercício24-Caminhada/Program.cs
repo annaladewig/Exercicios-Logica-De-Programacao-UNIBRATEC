31a5b60b4cbab0de522677ddbda22318e61014a5
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-05-19 08:37:28 -0300

    Adicionei o Exercício 24

---
+++ 1º Unidade/Exercício24-Caminhada/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício24_Caminhada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variáveis
+            int horaInicial, minutoInicial, horaFinal, minutoFinal;
+            double distanciaPercorrida,totalMinutos, distanciaPorMinuto;
+
+            //Entradas
+            try
+            {
+                Console.Write("Qual a hora inicial da caminhada (sem os minutos)? ");
+                horaInicial = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Qual o minuto inicial da caminhada? ");
+                minutoInicial = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Qual a hora final da caminhada (sem os minutos)? ");
+                horaFinal = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Qual o minuto final da caminhada? ");
+                minutoFinal = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Qual a distância total percorrida em metros?");
+                distanciaPercorrida = Convert.ToDouble(Console.ReadLine());
+
+            }
+            catch
+            {
+                Console.Write("Por favor, digite apenas NÚMEROS.");
+                Console.ReadKey();
+                return;
+            }
+            //Processamento
+            totalMinutos = ((horaFinal * 60) + minutoFinal) - ((horaInicial * 60) + minutoInicial);
+            distanciaPorMinuto = distanciaPercorrida / totalMinutos;
+
+            //Saída
+            Console.WriteLine("A quantidade total de tempo percorrido foi de {0} minutos.", totalMinutos);
+            Console.WriteLine("A distância percorrida foi de {0:F} metros por minuto.", distanciaPorMinuto);
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ 1º Unidade/Exercício24-Caminhada/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Exercício24-Caminhada")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Exercício24-Caminhada")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("07b44abe-ea89-4d57-bdff-32c3ef72c7b4")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
