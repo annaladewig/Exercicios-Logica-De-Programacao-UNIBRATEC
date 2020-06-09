3bfe2e93d37e09e15c51e4059eb2680f410f14d1
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-06-08 21:33:45 -0300

    Adicionei o Exercício 40

---
+++ 1º Unidade/Exercício40-ParOuImpar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício40_ParOuImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.Write("Digite um número inteiro: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("O número {0} é PAR.", numero);
+            }
+            else
+            {
+                Console.WriteLine("O número {0} é ÍMPAR.", numero);
+            }
+            Console.ReadKey();
+        }
+    }
+}
