8c522da31cb7be48ab9ef4e2574db8957e3695b3
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-06-11 00:36:21 -0300

    Adicionei o Exercício 43

---
+++ 1º Unidade/Exercício43-2ºTurnoEleicao/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício43_2ºTurnoEleicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int votosCandidato1, votosCandidato2, votosCandidato3;
+
+            Console.Write("Qual a quantidade de votos do Candidato 1? ");
+            votosCandidato1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Qual a quantidade de votos do Candidato 2? ");
+            votosCandidato2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Qual a quantidade de votos do Candidato 3? ");
+            votosCandidato3 = Convert.ToInt32(Console.ReadLine());
+
+            if ((votosCandidato1 + votosCandidato2 >= votosCandidato3) && 
+                (votosCandidato2 + votosCandidato3 >= votosCandidato1) && 
+                (votosCandidato1 + votosCandidato3 >= votosCandidato2))
+            {
+                Console.WriteLine("Haverá segundo turno.");
+            }
+            else
+            {
+                Console.WriteLine("NÃO haverá segundo turno.");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
