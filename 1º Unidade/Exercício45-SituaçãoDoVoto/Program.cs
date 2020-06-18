2495b223bfe4ccb77661f015b6199de56fe0f6df
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-06-18 12:10:03 -0300

    Adicionei o Exercício 45

---
+++ 1º Unidade/Exercício45-SituaçãoDoVoto/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício45_SituaçãoDoVoto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            Console.WriteLine("Qual a sua idade? ");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            if (idade == 16 || idade ==17)
+            {
+                Console.WriteLine("Voto OPCIONAL.");
+            }
+            else if (idade >=18 && idade <=65)
+            {
+                Console.WriteLine("Voto OBRIGATÓRIO.");
+            }
+            else if (idade > 65)
+            {
+                Console.WriteLine("Voto OPCIONAL.");
+            }
+            else // (idade <16)
+            {
+                Console.WriteLine("NÃO pode votar.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
