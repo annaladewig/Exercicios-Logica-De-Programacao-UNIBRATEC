de0e3448d746c2df41ee3a72f2b006fd04597eaf
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-06-19 19:21:47 -0300

    Adicionei o Exercício 46

---
+++ 1º Unidade/Exercício46-CalculoReajusteSalarial/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício46_CalculoReajusteSalarial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempoServico;
+            double salario, novoSalario;
+
+            Console.WriteLine("Tempo de Serviço (em anos): ");
+            tempoServico = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Salário: ");
+            salario = Convert.ToDouble(Console.ReadLine());
+
+            if (tempoServico >= 10 && salario > 3000)
+            {
+                novoSalario = salario + salario * 0.10;
+            }
+            else if (tempoServico <= 9 && salario > 3000)
+            {
+                novoSalario = salario + salario * 0.08;
+            }
+            else
+            {
+                novoSalario = salario + salario * 0.05;
+
+            }
+            Console.WriteLine("O novo salário será de {0:N}", novoSalario);
+            Console.ReadKey();
+        }
+    }
+}
