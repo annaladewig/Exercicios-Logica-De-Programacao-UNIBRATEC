c1939b09569794687c7a8d81c438b51122c4fa90
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-06-22 18:19:20 -0300

    Adicionei o Exercício 47

---
+++ 1º Unidade/Exercício47-CalculoParcelaPagaEmAtraso/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício47_CalculoParcelaPagaEmAtraso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorJuros, valorPagamento;
+        }
+    }
+}
