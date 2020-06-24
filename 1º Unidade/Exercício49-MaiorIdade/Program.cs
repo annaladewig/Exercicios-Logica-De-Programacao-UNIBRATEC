838c3c9b53af835c4b002df8e7ce9c83bc21444f
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-06-24 08:10:17 -0300

    Adicionei o Exercício 49

---
+++ 1º Unidade/Exercício49-MaiorIdade/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício49_MaiorIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador, idade = 0, numPessoasMaiores = 0;
+
+            for (contador = 1; contador <=10; contador++)
+            {
+                Console.WriteLine("Qual a {0}º idade? ",contador);
+                idade = Convert.ToInt32(Console.ReadLine());
+                if (idade>=18)
+                {
+                    numPessoasMaiores = numPessoasMaiores + 1;
+                }
+            }
+            Console.WriteLine("O número de pessoas maiores de idade foi {0}.", numPessoasMaiores);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
