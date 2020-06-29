7385774a478d7e9d4c357ead4132563542583901
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-06-29 10:34:37 -0300

    Adicionei o Exercício 52

---
+++ 1º Unidade/Exercício52-NomePessoaMaiorIdade/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício52_NomePessoaMaiorIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, nomeMaiorIdade = "";
+            int  contador, idade = 0, maiorIdade = 0;
+
+            for (contador = 1; contador <= 5; contador++)
+            {
+                Console.WriteLine("Qual o nome da {0}º pessoa? ", contador);
+                nome = Console.ReadLine();
+                Console.WriteLine("Qual a idade da {0}º pessoa?", contador);
+                idade = Convert.ToInt32(Console.ReadLine());
+                if (idade > maiorIdade)
+                {
+                    maiorIdade = idade;
+                    nomeMaiorIdade = nome;
+                }
+            }
+            Console.WriteLine("A pessoa com a maior idade é {0}.", nomeMaiorIdade);
+            Console.ReadKey();
+
+        }
+    }
+}
