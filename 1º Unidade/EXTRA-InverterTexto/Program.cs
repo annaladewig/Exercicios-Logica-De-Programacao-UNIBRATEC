2bda1f9e41a22d35e27cb4e883cebcef10861ee6
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-07-07 08:12:43 -0300

    Adicionei o Exercício EXTRA-InverterTexto

---
+++ 1º Unidade/EXTRA-InverterTexto/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXTRA_InverterTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+            int contador;
+
+            Console.Write("Escreva um texto: ");
+            texto = Console.ReadLine();
+            texto.ToUpper();
+
+            for (contador = (texto.Length) - 1; contador >= 0; contador = contador - 1)
+            {
+                Console.Write(texto.Substring(contador, 1));
+            }
+            Console.ReadKey();
+        }
+    }
+}
