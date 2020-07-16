26bc369b05ac56b9350ef4af89e515f30d1fce28
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-07-16 12:26:53 -0300

    Adicionei o Exercício EXTRA-VogalOuConsoante

---
+++ 1º Unidade/EXTRA-VogalOuConsoante/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXTRA_VogalOuConsoante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string letra = "";
+            while (true)
+            {
+                Console.WriteLine("Digite um letra: ");
+                letra = Console.ReadLine();
+                letra = letra.ToUpper();
+                if ((string.Compare(letra, "A") == -1) || string.Compare(letra, "Z") == 1)
+                {
+                    Console.WriteLine("digite apenas letras!!!");
+                    Console.ReadKey();
+                    continue;
+                }
+                break;
+            }
+            if (letra == "A" || letra == "E" || letra == "I" || letra == "O" || letra == "U")
+            {
+                Console.WriteLine("Vogal");
+            }
+            else
+            {
+                Console.WriteLine("Consoante");
+            }
+            Console.ReadKey();
+        }
+    }
+}
