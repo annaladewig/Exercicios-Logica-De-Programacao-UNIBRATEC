f3c456e1d9eb8209a9c42c13b7eb214102df8097
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-07-17 15:26:40 -0300

    Adicionei ProvaAnterior-Questão2VogalConsoante

---
+++ 1º Unidade/ProvaAnterior-Questão2VogalConsoante/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaAnterior_Questão2VogalConsoante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, ultimaLetra;
+            Console.WriteLine("Escreva um nome: ");
+            nome = Console.ReadLine();
+
+            ultimaLetra = nome.Substring((nome.Length)-1, 1);
+            ultimaLetra = ultimaLetra.ToUpper();
+            if (ultimaLetra != "A" && ultimaLetra != "E" && ultimaLetra != "I" && ultimaLetra != "O" && ultimaLetra != "U")
+            {
+                Console.WriteLine("O nome termina com CONSOANTE.");
+            }
+            else
+            {
+                Console.WriteLine("O nome termina com VOGAL.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
