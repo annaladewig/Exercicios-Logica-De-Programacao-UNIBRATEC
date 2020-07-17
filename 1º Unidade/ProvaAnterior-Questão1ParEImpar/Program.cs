8fea37e48ad5d66095886e6f41237664891918f1
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-07-17 15:25:01 -0300

    Adicionei ProvaAnterior-Questão1ParEImpar

---
+++ 1º Unidade/ProvaAnterior-Questão1ParEImpar/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaAnterior_Questão1ParEImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero, numPares = 0, numImpares = 0;
+
+            for(int contador = 1; contador <= 6; contador++)
+            {
+                Console.WriteLine("Informe um número: ");
+                numero = Convert.ToDouble(Console.ReadLine());
+
+                numero = Convert.ToInt32(numero);
+            
+                if (numero % 2 == 0)
+                {
+                    numPares = numPares + 1;
+                }
+                else
+                {
+                    numImpares = numImpares + 1;
+                }
+            }
+            if (numPares > numImpares)
+            {
+                Console.WriteLine("Existem mais números PARES.");
+            }
+            else if (numImpares > numPares)
+            {
+                Console.WriteLine("Existem mais números ÍMPARES.");
+            }
+            else
+            {
+                Console.WriteLine("Os números ímpares e pares são IGUAIS.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
