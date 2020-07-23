6529204375fe30afe602cf324202ade16dbdafb7
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-07-23 11:20:09 -0300

    Adicionei Exercício01-Matrizes

---
+++ 2° Unidade/Exercício01-Matrizes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício01_Matrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] idade = new int[3, 3];
+            int contador18 = 0, contador65 = 0;
+
+            for (int lin = 0; lin < 3; lin++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    Console.WriteLine("Escreva a idade: ");
+                    idade[lin, col] = Convert.ToInt32(Console.ReadLine());
+
+                    if (idade[lin, col] >= 18)
+                    {
+                        contador18 = contador18 + 1;
+                    }
+
+                    if (idade[lin, col] >= 65)
+                    {
+                        contador65 = contador65 + 1;
+                    }
+                }
+
+            }
+            Console.WriteLine("As idades iguais ou maiores de 18 anos são: " + contador18);
+            Console.WriteLine("As idades iguais ou maiores de 65 anos são: " + contador65);
+            Console.ReadKey();
+        }
+    }
+}
