a87d3cd92aa2ed65f6b779746e7b9cd411bd5e31
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-07-27 11:24:53 -0300

    Adicionei Exercício01-Vetor

---
+++ 2° Unidade/Exercício01-Vetor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício01_Vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double [] nota = new double[5];
+            double acumulado = 0, mediaGeral;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Nota do {0}º aluno: ", i+1);
+                nota[i] = Convert.ToDouble(Console.ReadLine());
+                acumulado = acumulado + nota[i];
+            }
+            mediaGeral = acumulado / 5;
+            Console.WriteLine("A média geral foi de {0:F}.", mediaGeral);
+            Console.ReadKey();
+
+        }
+    }
+}
