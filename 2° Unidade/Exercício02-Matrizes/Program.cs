342458ac234e32fffe1c893f3a82aa883702e708
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-07-28 14:12:32 -0300

    Adicionei Exercício02-Matrizes

---
+++ 2° Unidade/Exercício02-Matrizes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício02_Matrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] num = { { 20, 30, 40 }, { 05, 10, 15 }, { 12, 23, 54 } };
+
+            Console.Write("Os números pares são:  ");
+            for (int lin = 0; lin < 3; lin++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+
+                    if (num[lin, col] % 2 == 0)
+                    {
+                        Console.Write(" " + num[lin, col] + " ");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
