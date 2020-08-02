5485aee46895b661150bbef05b2eb6d6d6e2b215
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-08-02 19:44:35 -0300

    Adicionei Exercício03-Matrizes

---
+++ 2° Unidade/Exercício03-Matrizes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício03_Matrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] num = { { 20, 30, 40 }, { 05, 10, 15 }, { 12, 23, 54 } };
+
+            Console.Write("Os números das linhas pares são:  ");
+            for (int lin = 0; lin < 3; lin++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+
+                    if (lin % 2 == 0)
+                    {
+                        Console.Write(" " + num[lin, col] + " ");
+                    }
+                }
+            }
+            Console.ReadKey();
+
+            /* OU
+             * int[,] num = { { 20, 30, 40 }, { 05, 10, 15 }, { 12, 23, 54 } };
+
+            Console.Write("Os números das linhas pares são:  ");
+            for (int lin = 0; lin < 3; lin=lin+2)  
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                        Console.Write(" " + num[lin, col] + " ");
+                }
+            }
+            Console.ReadKey();
+
+            E, se fosse pra linhas ímpares, era só começar da linha "1" e incrementar de 2 em 2 também.
+            */
+        }
+    }
+}
+            
