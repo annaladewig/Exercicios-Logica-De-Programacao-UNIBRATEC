930205332407f53adfa6774e5fdb07298c1a802b
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-08-04 16:13:42 -0300

    Adicionei Exercício03-Vetor

---
+++ 2° Unidade/Exercício03-Vetor/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício03_Vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nota = new double[5];
+            double acumulado = 0, mediaGeral;
+            int superiorMediaGeral = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Nota do {0}º aluno: ", i + 1);
+                nota[i] = Convert.ToDouble(Console.ReadLine());
+                acumulado = acumulado + nota[i];
+            }
+
+            mediaGeral = acumulado / 5;
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (nota[i] >= mediaGeral)
+                {
+                    superiorMediaGeral = superiorMediaGeral + 1;
+                }
+            }
+            Console.WriteLine("A média geral foi de {0:F}.", mediaGeral);
+            Console.WriteLine("Houveram {0} notas iguais ou superiores à média geral.", superiorMediaGeral);
+            Console.Write("As notas maiores que a média geral foram: ");
+            for (int i = 0; i < 5; i++)
+            {
+                if (nota[i] >= mediaGeral)
+                {
+                    Console.Write(" " + nota[i] + " ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
