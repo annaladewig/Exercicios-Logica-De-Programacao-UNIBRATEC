5c45dc6b126faf708f9cee44aff6059b1621490d
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-08-08 07:19:40 -0300

    Adicionei Exercício05-Matrizes

---
+++ 2° Unidade/Exercício05-Matrizes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício05_Matrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] num = { { 20, 30, 40 }, { 05, 10, 15 }, { 12, 23, 54 } };
+
+            Console.Write("Os números da diagonal secundária são:  ");
+
+            for (int lin = 0; lin < 3; lin++)
+            {
+                for (int col = num.Length - 1; col >= 0; col--)
+                {
+                    if (lin + col == 2)
+
+                        Console.Write(" " + num[lin, col] + " ");
+
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
