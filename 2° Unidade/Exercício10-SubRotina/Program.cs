06b36f452a4ce70e4fc4c7accdc9f096061a48e9
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-08-23 16:42:59 -0300

    Adicionei Exercício10-SubRotina

---
+++ 2° Unidade/Exercício10-SubRotina/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício10_SubRotina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                double valorPedido, distanciaEntrega;
+
+                Console.WriteLine("Digite o valor do pedido: ");
+                valorPedido = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Digite a distância de entrega: ");
+                distanciaEntrega = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("O valor do frete é:{0}", CalculoFrete(distanciaEntrega));
+                Console.ReadKey();
+            }
+        }
+            static double CalculoFrete(double distancia)
+            {
+                double valorFreteFinal = 0;
+
+                if (distancia > 0 && distancia <= 1000)
+                {
+                    valorFreteFinal = 20 + (distancia * 0.50);
+                }
+                if (distancia > 1000)
+                {
+                    valorFreteFinal = 20 + (distancia * 0.40);
+                }
+                return valorFreteFinal;
+            }
+        
+    }
+
+}
+    
+
